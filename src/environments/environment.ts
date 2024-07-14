42ab8610b28976090ead0549b8a2930142f26f3f
DavidAyad97 <davidayad88@gmail.com>
2024-07-14 17:33:16 +0300

    fix bug in edit book and add dummy data

---
+++ src/environments/environment.ts
@@ -0,0 +1,11 @@
+export const environment = {
+    production: false,
+    firebaseConfig : {
+        apiKey: "AIzaSyDutDv4rsC_Vw__qpRVJ9sSZ4K6KoSZEJg",
+        authDomain: "acore-8497e.firebaseapp.com",
+        projectId: "acore-8497e",
+        storageBucket: "acore-8497e.appspot.com",
+        messagingSenderId: "789078148951",
+        appId: "1:789078148951:web:82c6610cc0b2ff06b4e012"
+      }
+};
